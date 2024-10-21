b45a1228bf672851efc7774574808e7ffa3c91db
Jair Balcazar <jairbalcazar.jb@gmail.com>
2024-10-20 19:40:03 -0500

    update seed

--- src/prisma/seed.js
+++ src/prisma/seed.js
@@ -1,10 +1,12 @@
 import { PrismaClient, ROLE } from '@prisma/client';
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function main() {
   const email = process.env.DEFAULT_EMAIL_ADMIN;
   const password = process.env.DEFAULT_PASSWORD_ADMIN;
+  const hashedPassword = await bcrypt.hash(password, 10);
   const existUser = await prisma.user.findMany({
     where: {
       email,
